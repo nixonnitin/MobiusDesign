c02f6259e32b7e8688c84f5d84b58eea8a2f488e
nixonnitin <37650134+nixonnitin@users.noreply.github.com>
2018-04-17 20:01:06 +0530

    Reward Section

--- iOS/Views/CheckoutView/Cells/CheckoutPostcodeCountryCell.designer.cs
+++ iOS/Views/CheckoutView/Cells/CheckoutPostcodeCountryCell.designer.cs
@@ -8,13 +8,75 @@ using Foundation;
 using System;
 using System.CodeDom.Compiler;
 
-namespace Mobius.iOS.Views.CheckoutView.Cells
+namespace Mobius.iOS.Views
 {
     [Register ("CheckoutPostcodeCountryCell")]
     partial class CheckoutPostcodeCountryCell
     {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIImageView ImageGlobe { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIImageView ImageMailBox { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField TextFieldCountry { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField TextFieldPostcode { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIView ViewCountry { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIView ViewMainPostCodeCountry { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIView ViewPostcode { get; set; }
+
         void ReleaseDesignerOutlets ()
         {
+            if (ImageGlobe != null) {
+                ImageGlobe.Dispose ();
+                ImageGlobe = null;
+            }
+
+            if (ImageMailBox != null) {
+                ImageMailBox.Dispose ();
+                ImageMailBox = null;
+            }
+
+            if (TextFieldCountry != null) {
+                TextFieldCountry.Dispose ();
+                TextFieldCountry = null;
+            }
+
+            if (TextFieldPostcode != null) {
+                TextFieldPostcode.Dispose ();
+                TextFieldPostcode = null;
+            }
+
+            if (ViewCountry != null) {
+                ViewCountry.Dispose ();
+                ViewCountry = null;
+            }
+
+            if (ViewMainPostCodeCountry != null) {
+                ViewMainPostCodeCountry.Dispose ();
+                ViewMainPostCodeCountry = null;
+            }
+
+            if (ViewPostcode != null) {
+                ViewPostcode.Dispose ();
+                ViewPostcode = null;
+            }
         }
     }
 }
